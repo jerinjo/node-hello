8e020c806b222ee2c770ab0b68d4c985c82e069e
jerinjo <jerin.john2@cognizant.com>
2017-06-07 11:24:11 +0530

    sdfhklsj

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ var filter = {
   order: 'asc'
 };
 
+let value;
+
 // Get all the questions (http://api.stackexchange.com/docs/questions)
 context.questions.questions(filter, function(err, results){
   if (err) throw err;
   
+  value=results.items;
   console.log(results.items);
   console.log(results.has_more);
 });
+
 
 var port = process.env.PORT || 8080;
 
@@ -28,7 +32,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+  response.end("Hello " + user_name + "!"+value);
 });
 
 app.listen(port);
